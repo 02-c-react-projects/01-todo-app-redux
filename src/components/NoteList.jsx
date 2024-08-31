f77a2292e44cb2ec9c90190f3784de1ac0938295
hiteshwar <hiteshwarkaim@gmail.com>
2024-08-31 20:50:04 +0530

    change another note state in store (delete note)

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,16 +1,19 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteNote } from "../../redux/actions/noteActions";
 
 
 function NoteList() {
     const notes=useSelector(state => state.noteReducer.notes);
+    const dispatch = useDispatch();
+
   return (
     <div className="container">
     <ul>
       {notes.map((note,index) => (
-        <li>
+        <li key={index}>
             <span>{note.createdOn.toLocaleDateString()}</span>&nbsp;&nbsp;
             <span>{note.text}</span>&nbsp;&nbsp;
-            <button>Delete</button>
+            <button onClick={() => {dispatch(deleteNote(index))}}>Delete</button>
             </li>
       ))}
     </ul>
