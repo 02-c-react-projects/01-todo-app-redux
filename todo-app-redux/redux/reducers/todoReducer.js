6de448dc3d22cc4c8db4da5639017321aefb8261
hiteshwar <hiteshwarkaim@gmail.com>
2024-08-31 11:33:05 +0530

    create reducers in redux

---
+++ todo-app-redux/redux/reducers/todoReducer.js
@@ -0,0 +1,36 @@
+import { ADD_TODO, TOGGLE_TODO } from '../actions/todoActions'
+
+const initialState = {
+    todos: [],
+}
+function todoReducer(state = initialState, action) {
+    switch (action.type) {
+        
+        case 'ADD_TODO':
+            return {
+                ...state,
+                todos:[
+                    ...state.todos,
+                    {
+                        id: state.todos.length,
+                        text: action.text,
+                        isCompleted: false
+                    }
+                ]
+            }
+
+        case 'TOGGLE_TODO':
+            return {
+                ...state,
+                todos: state.todos.map((todo, index) => {
+                    if (index === action.index) {
+                        todo.isCompleted = !todo.isCompleted
+                    }
+                    return todo
+                })
+            }
+
+        default:
+            return state
+    }
+}
